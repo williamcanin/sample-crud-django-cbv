0ec99ab5c2eb23b18954476478bc4f009594d402
William C. Canin <william.costa.canin@gmail.com>
2020-06-08 20:15:42 -0300

    Update - D:08062020-H:201542

--- person/admin.py
+++ person/admin.py
@@ -4,4 +4,18 @@ from .models import PersonModel
 # Register your models here.
 
 
-admin.site.register(PersonModel)
+class PersonAdmin(admin.ModelAdmin):
+    exclude = []
+    readonly_fields = ("age",)
+    fieldsets = (
+        ("Personal Data", {
+            "fields": (
+                ("first_name", "last_name"), ("age")
+            ),
+        }),
+    )
+    list_display = ("id", "first_name", "last_name", "age")
+    search_fields = ("id", "last_name")
+
+
+admin.site.register(PersonModel, PersonAdmin)
